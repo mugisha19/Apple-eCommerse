b7b7cf20422f04a78bb5b61487b1ecbcd438fd1b
Liad <habiyaadolphe19@gmail.com>
2024-05-23 10:43:04 +0200

    section 2&3 functionality

--- script.js
+++ script.js
@@ -106,3 +106,15 @@ const slideshow = () => {
 
 slideshow ();
 // End Of slideshow
+
+// section 3sectio
+
+const section3Content = document.querySelector('.section-3-content');
+
+window.addEventListener('scroll', () => {
+    if (window.pageYOffset + window.innerHeight >= section3Content.offsetTop + section3Content.offsetHeight / 2) {
+        section3Content.classList.add('change')
+    }
+})
+
+// end of  section 3
